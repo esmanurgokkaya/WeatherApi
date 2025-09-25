c58cbc2b2e0e66596b0b84ea7696115252517912
Esma <esmanurgkky10@gmail.com>
2025-09-25 16:55:36 +0300

    feat: implement AuthService with user registration functionality

---
+++ src/services/auth.service.js
@@ -0,0 +1,28 @@
+import UserModel from "../models/user.model.js";
+import bcrypt from 'bcrypt';
+
+class AuthService {
+
+    async register(data) {
+        
+        // email control
+        const existingUser = await UserModel.getUserByEmail(data.email);
+
+        if(existingUser){
+            throw new Error("Email already in use");
+        }
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(data.password, 10);
+
+        // create user
+        const User = await UserModel.createUser({
+            ...data,
+            password: hashedPassword,
+        });
+
+        return User;
+    }
+}
+export default new AuthService();
+
