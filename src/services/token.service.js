322cc8025c12a79d7793998db3416ebd5b5b1e2e
Esma <esmanurgkky10@gmail.com>
2025-09-28 17:16:34 +0300

    feat: implement user controller with CRUD operations and token validation

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -3,8 +3,10 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import ms from "ms";
 dotenv.config();
+// decode işlemini  burda yapabilir miyiz?
 
 class TokenService {
+
   async generateAccessToken(userId, userEmail) {
     const accessToken = jwt.sign(
       { userId, userEmail },
@@ -36,7 +38,7 @@ class TokenService {
     const expiresIn = process.env.JWT_REFRESH_EXPIRES_IN || "7d";
     const refreshToken = jwt.sign(
       { userId, userEmail },
-      process.env.REFRESH_TOKEN_SECRET,
+      process.env.JWT_REFRESH_SECRET,
       { expiresIn }
     );
     // Calculate expiresAt
