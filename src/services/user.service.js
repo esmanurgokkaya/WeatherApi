c4dbc3ed99b4a95a044c779dbb8ffae0fbf0d692
Esma <esmanurgkky10@gmail.com>
2025-09-28 17:15:56 +0300

    feat: implement user service with methods for profile retrieval, update, password management, and deletion

---
+++ src/services/user.service.js
@@ -0,0 +1,59 @@
+import UserModel from '../models/user.model.js';
+import bcrypt from 'bcrypt';
+
+class  UserService {
+
+    async getUserProfile(id){
+        const user = await UserModel.getUserById(id);
+
+        if(!user){
+            throw new Error("User not found");
+        }
+
+        return user;
+    }
+
+    async updateUser(id, data){
+        const user = await UserModel.getUserById(id);
+
+        if(!user){
+            throw new Error("User not found");
+        }
+
+        return await UserModel.updateUser(id, data);
+    }
+
+    async updatePassword(id, oldPassword, newPassword){
+        const user = await UserModel.getUserById(id);
+        if(!user){
+            throw new Error("User not found");
+        }
+        
+        const isPasswordValid = await bcrypt.compare(oldPassword, user.password);
+
+        if(!isPasswordValid){
+            throw new Error("Old password is incorrect");
+        }
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        newPassword = hashedPassword;
+
+        return await UserModel.updateUser(id, { password: newPassword });
+    }
+
+    async deleteUser(id){
+        const user = await UserModel.getUserById(id);
+
+        if(!user){
+            throw new Error("User not found");
+        }
+    
+        return await UserModel.deleteUser(id);
+    }
+
+    async getAllUsers(){
+        return await UserModel.getAllUsers();
+    }
+}
+
+export default new UserService();
