0ed367f33559aa9d243a2e1bd65d30d2f45f1bac
Esma <esmanurgkky10@gmail.com>
2025-10-02 17:43:24 +0300

    fix: remove duplicate route for retrieving all locations

--- src/routes/location.route.js
+++ src/routes/location.route.js
@@ -3,9 +3,9 @@ import { authenticate } from '../middlewares/auth.middleware.js';
 import express from 'express';
 const router = express.Router();
 
+router.get('/all', authenticate, LocationController.getAllLocationsByUserId);
 router.post('/', authenticate, LocationController.createLocation);
 router.get('/:id', authenticate, LocationController.getLocationById);
-router.get('/all/', authenticate, LocationController.getAllLocationsByUserId);
 router.put('/:id', authenticate, LocationController.updateLocation);
 router.delete('/:id', authenticate, LocationController.deleteLocation);
 
