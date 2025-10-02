4583b42216768af510a8aab2048d009d72eb0ab8
Esma <esmanurgkky10@gmail.com>
2025-10-02 17:12:33 +0300

    fix: update route for retrieving all locations to use a consistent endpoint

--- src/routes/location.route.js
+++ src/routes/location.route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/', authenticate, LocationController.createLocation);
 router.get('/:id', authenticate, LocationController.getLocationById);
-router.get('/user/:user_id', authenticate, LocationController.getAllLocationsByUserId);
+router.get('/all/', authenticate, LocationController.getAllLocationsByUserId);
 router.put('/:id', authenticate, LocationController.updateLocation);
 router.delete('/:id', authenticate, LocationController.deleteLocation);
 
