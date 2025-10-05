e4d0d2157f960eda3c8f426fc5c3609ed9e7542a
Esma <esmanurgkky10@gmail.com>
2025-10-05 23:56:17 +0300

    feat: add alert routes for managing user alerts

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -3,6 +3,7 @@ import AuthRoutes from "./auth.route.js";
 import UserRoutes from "./user.route.js";
 import LocationRoutes from "./location.route.js";
 import WeatherRoutes from "./weather.route.js";
+import AlertRoutes from "./alert.route.js";
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import path from 'path';
@@ -16,5 +17,6 @@ router.use("/auth", AuthRoutes);
 router.use("/profile", UserRoutes);
 router.use("/location", LocationRoutes);
 router.use("/weather", WeatherRoutes);
+router.use("/alerts", AlertRoutes);
 
 export default router;
