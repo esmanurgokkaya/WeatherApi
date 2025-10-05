2c1fc3904a4ddf894424e371e105274e0dbc24c2
Esma <esmanurgkky10@gmail.com>
2025-10-05 23:04:36 +0300

    feat: enhance getCurrentWeather method with caching mechanism and locationId parameter

--- src/services/weather.service.js
+++ src/services/weather.service.js
@@ -1,30 +1,40 @@
 import axios from "axios";
+import WeatherModel from '../models/weather.model.js';
 
 const OPENWEATHER_URL = "https://api.openweathermap.org/data/2.5";
 const API_KEY = process.env.OPENWEATHER_API_KEY;
 
 class WeatherService {
-    static async getCurrentWeather(lat, lon) {
+    static async getCurrentWeather(lat, lon, locationId) {
+        // Önce cache kontrolü
+        const cache = await WeatherModel.getCache({ locationId, type: "current" });
+        if (cache) {
+            const now = new Date();
+            const age = (now - cache.fetchedAt) / 1000;
+            if (age < 300) { // 5 dakika
+                return cache.data;
+            }
+        }
+
+        // API'den veri çek
         const response = await axios.get(`${OPENWEATHER_URL}/weather`, {
-            params: {
-                lat,
-                lon,
-                appid: API_KEY,
-                units: "metric",
-            },
+            params: { lat, lon, appid: API_KEY, units: "metric" },
         });
-        const data = response.data;
-        return {
-            location: data.name,
-            country: data.sys.country,
-            temperature: data.main.temp,
-            feels_like: data.main.feels_like,
-            weather: data.weather[0].main,
-            description: data.weather[0].description,
-            icon: data.weather[0].icon,
-            humidity: data.main.humidity,
-            wind_speed: data.wind.speed,
+        const data = {
+            location: response.data.name,
+            country: response.data.sys.country,
+            temperature: response.data.main.temp,
+            feels_like: response.data.main.feels_like,
+            weather: response.data.weather[0].main,
+            description: response.data.weather[0].description,
+            icon: response.data.weather[0].icon,
+            humidity: response.data.main.humidity,
+            wind_speed: response.data.wind.speed,
         };
+
+        // Cache'e kaydet
+        await WeatherModel.createOrUpdateCache({ locationId, type: "current", data });
+        return data;
     }   
     static async getHourlyForecast(lat, lon) {
         const response = await axios.get(`${OPENWEATHER_URL}/forecast`, {
